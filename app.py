76caac012dbb09120b080f17717554ce26ba8386
SourabhR23 <rasal.sourabh23@gmail.com>
2021-08-01 11:56:22 +0530

    web-app

--- app.py
+++ app.py
@@ -10,4 +10,15 @@ st.title("""
 
 # Sidebar header
 st.sidebar.header('User Input Features')
-st.sidebar.markdown('[Example CSV Input file]()')
+st.sidebar.markdown('[Example CSV Input file](https://github.com/SourabhR23/teleco-churn-webapp/blob/master/Telecom/churn_example.csv)')
+
+# Collect user input features into dataframe
+uploaded_file = st.sidebar.file_uploader('Upload your input CSV file', type = ['csv'])
+if uploaded_file is not True:
+    input_df = pd.read_csv(uploaded_file)
+#else:
+ #   def user_input_features():
+
+
+
+
