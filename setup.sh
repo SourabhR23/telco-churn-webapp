76f49f2b22126ee35144050b3bf9685d87e48592
SourabhR23 <rasal.sourabh23@gmail.com>
2021-08-02 09:21:51 +0530

    new update

--- setup.sh
+++ setup.sh
@@ -5,11 +5,5 @@ echo "\
 port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
-[theme]\n\
-primaryColor="#cbadba"\n\
-backgroundColor="#173e43"\n\
-secondaryBackgroundColor="#3fb0ac"\n\
-textColor="#fae596"\n\
-font="serif"\n\
 \n\
 " > ~/streamlit/config.toml
